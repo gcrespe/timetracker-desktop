c56e288746aa4b63bf47a40684e6a1ccc97c925d
Giuliano Crespe <gcrespe3@hotmail.com>
2021-05-06 23:11:13 -0300

    components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import time from './assets/timeManagement.svg'
+import Button from './components/Button/Button.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App">
         <div className="LoginArea">
+          <Button />
         </div>
         <div className="imageArea">
           <img src={time} style={{width: '70%', height: '70%', paddingTop: "15%"}}/>
